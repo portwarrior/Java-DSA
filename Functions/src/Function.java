5cfe3cce2c915cc2befd801dfb8231c3419b7d49
pixilised <bilaltosif123@gmail.com>
2024-03-03 21:09:59 +1100

    Commit 003
    Functions revision

---
+++ Functions/src/Function.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Function {
+
+    public static void main(String[] args) {
+        //q:take input of 2 numbers and print the sum
+        System.out.println(sum());
+    }
+    static int sum(){
+        Scanner in=new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int b=in.nextInt();
+        System.out.println("Enter the second number");
+        int c=in.nextInt();
+        return b+c;
+    }
+}
---
+++ Functions/src/Greeting.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Greeting {
+    public static void main(String[] args) {
+        Scanner in=new Scanner(System.in);
+        System.out.println("Whats your name: ");
+        String b=in.next();
+        System.out.println(greetuser(b));
+        greet();
+    }
+
+    static String greetuser(String v){
+        String user="Hi " + v;
+        return user;
+    }
+
+    static void greet(){
+        System.out.println("Hello World, I am present within a function");
+    }
+}
