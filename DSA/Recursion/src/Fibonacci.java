237f6806d556555ff9a9ab49ebf51acc15c6f414
pixilised <bilaltosif123@gmail.com>
2024-04-14 14:45:50 +1000

    Fix Fibonacci sequence calculation in Fibonacci.java

--- DSA/Recursion/src/Fibonacci.java
+++ DSA/Recursion/src/Fibonacci.java
@@ -2,12 +2,12 @@ package Recursion.src;
 
 public class Fibonacci {
     public static void main(String[] args) {
-        int x=Fibo(5);
+        int x=Fibo(6);
         System.out.println(x);
     }
     public static int Fibo(int n){
         if(n-1==0){
-            return 0;
+            return 1;
         }
         if(n-2==0){
             return 1;
