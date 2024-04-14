99c7fed5e6107cacb0073194976e9189b034f836
pixilised <bilaltosif123@gmail.com>
2024-04-14 15:54:39 +1000

    Refactor Fibonacci class to include Fibo2 method

--- DSA/Recursion/src/Fibonacci.java
+++ DSA/Recursion/src/Fibonacci.java
@@ -4,6 +4,8 @@ public class Fibonacci {
     public static void main(String[] args) {
         int x=Fibo(6);
         System.out.println(x);
+        int y=Fibo2(6);
+        System.out.println(y);
     }
     public static int Fibo(int n){
         if(n-1==0){
@@ -11,6 +13,13 @@ public class Fibonacci {
         }
         if(n-2==0){
             return 1;
+        }
+        return Fibo(n-1)+Fibo(n-2);
+    }
+
+    public static int Fibo2(int n){
+        if(n<2){
+            return n;
         }
         return Fibo(n-1)+Fibo(n-2);
     }
